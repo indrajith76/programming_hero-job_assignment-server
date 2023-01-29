ead3084c8caaf9ae0062a8a8cb50c0fda3ea2b1f
indrajith76 <indrajithgoswami@gmail.com>
2023-01-29 21:13:19 +0600

    initialize mongodb

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${USER_Name}:${USER_PASSWORD}@cluster0.w2fg5h5.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.w2fg5h5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
 
 async function run() {
   try {
-    const billingCollection = client
-      .db("phero-job-assignment")
-      .collection("billingList");
+    const billingCollection = client.db("phero-job-assignment").collection("billingList");
 
     
 
